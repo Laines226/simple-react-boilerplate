8738626e872c02f3ac1e6137be4374de2445b75a
Linus Niedermaier <Linus.Niedermaier@citrix.com>
2017-03-07 17:16:15 +0100

    minimize package.json

--- src/components/myCheckbox.spec.js
+++ src/components/myCheckbox.spec.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import MyCheckbox from './mycheckbox';
+import MyCheckbox from './myCheckbox';
 
 describe("<MyComponent>", function() {
   it("renders the input", function() {
     const wrapper = shallow(<MyCheckbox />);
-    
     expect(wrapper.find('.myInput')).toHaveLength(1);   
   });
 
